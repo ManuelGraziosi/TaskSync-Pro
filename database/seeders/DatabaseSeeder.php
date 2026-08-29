7e84aa013245ad99ec7eac1ea4725eedf8c89a30
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-08-29 23:16:27 +0200

    feat: Pivot Team User created (wip)+ Env.example edited + DBSeeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,22 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        if (! env('DEFAULT_ADMIN_PASSWORD') || ! env('DEFAULT_USER_PASSWORD')) {
+            $this->command->error('Le password di default non sono state definite nel file .env. Impossibile eseguire il seeder.');
+
+            return;
+        }
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make(env('DEFAULT_USER_PASSWORD')),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make(env('DEFAULT_ADMIN_PASSWORD')),
         ]);
     }
 }
