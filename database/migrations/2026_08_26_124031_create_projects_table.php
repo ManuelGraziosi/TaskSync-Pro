b1e6a87d66c0c3be9368f71c8edfa0ecadeee7dd
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-08-29 23:15:25 +0200

    feat: Project Resources - Model, controller, migration and seeder created (wip)

--- database/migrations/2026_08_26_124031_create_projects_table.php
+++ database/migrations/2026_08_26_124031_create_projects_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+
+            $table->string('team_id');
+            $table->string('name');
+            $table->text('description');
+
             $table->timestamps();
         });
     }
